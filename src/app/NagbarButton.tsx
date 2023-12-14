8fb1a4c9da0b79f7187a5d89d5f2b43bb61bce86
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2023-12-14 21:42:22 +0700

    :sob:

--- src/app/NagbarButton.tsx
+++ src/app/NagbarButton.tsx
@@ -1,12 +1,12 @@
-
+import Link from 'next/link'
 export default function NagbarButton({children,href}:{children:any,href:string}){
     return (
         <>
-            <a href={href}>
+            <Link href={href}>
             <button className="rounded-lg border-dark-text border-2 px-3 hover:bg-dark-text hover:text-red-500 transition-colors">
                 {children}
             </button>
-            </a>
+            </Link>
         </>
     )
 }
